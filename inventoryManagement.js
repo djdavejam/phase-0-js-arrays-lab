8e7583ecd3b349f8a6f7ca9bb3a12234f340a51f
David James <davidanthoneyjames@gmail.com>
2025-07-12 12:31:48 -0400

    This is the Js Arrays Lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,8 +1,26 @@
-// Write your code here
+let products = [ "Laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(newProduct) {
+  products.push(newProduct);
+}
+
+function updateProductName(position, newName) {
+  if (position >= 0 && position < products.length) {
+    products[position] = newName;
+  } else {
+    console.error("Invalid position");
+  }
+}
+
+function removeLastProduct() {
+  products.pop();
+}
 
 
-
-// Export the necessary parts for testing
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
